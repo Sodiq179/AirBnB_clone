36843d38ad0e625be53804926802c5eaf25f9f7c
Sodiq <65002222+Sodiq179@users.noreply.github.com>
2022-09-12 20:08:12 +0100

    [Update]: Adding User class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,35 +15,39 @@ class FileStorage:
 
     __file_path = 'file.json'
     __objects = {}
-    dict = {"BaseModel": BaseModel, "Review": Review, "Place": Place,
-            "State": State, "Amenity": Amenity, "City": City, "User": User}
+    class_dict = {"BaseModel": BaseModel, "User": User, "Place": Place,
+                  "Amenity": Amenity, "City": City, "Review": Review,
+                  "State": State}
 
     def all(self):
-        '''Returns dictionary of the object instance'''
+        '''Return dictionary of <class>.<id> : object instance'''
         return self.__objects
 
     def new(self, obj):
-        '''Adds new object, obj, to the dictionary'''
+        '''Add new obj to existing dictionary of instances'''
         if obj:
-            key = f'{obj.__class__.__name__}.{obj.id}'
+            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
         '''Save obj dictionaries to json file'''
-        dict = {}
+        my_dict = {}
 
         for key, obj in self.__objects.items():
-            dict[key] = obj.to_dict()
-        with open(self.__file_path, 'w') as file:
-            json.dump(dict, file)
+            '''if type(obj) is dict:
+            my_dict[key] = obj
+            else:'''
+            my_dict[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(my_dict, f)
 
     def reload(self):
-        '''Converts obj dicts back to instances if json file exists'''
+        '''If json file exists, convert obj dicts back to instances'''
         try:
-            with open(FileStorage.__file_path, 'r') as file:
-                new_obj = json.load(file)
+            with open(self.__file_path, 'r') as f:
+                new_obj = json.load(f)
             for key, val in new_obj.items():
-                obj = self.dict[val['__class__']](**val)
+                obj = self.class_dict[val['__class__']](**val)
                 self.__objects[key] = obj
         except FileNotFoundError:
             pass
